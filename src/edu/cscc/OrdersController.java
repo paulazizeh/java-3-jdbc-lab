3f9acf92bc26686fe374c845fa92be673f4c0a0c
Paul Azizeh <pazizeh@WFIL004.cscc.local>
2021-01-25 14:34:13 -0500

    Updated OrdersController, OrdersCreate and OrdersIndex Java files.

--- src/edu/cscc/OrdersController.java
+++ src/edu/cscc/OrdersController.java
@@ -5,9 +5,8 @@ import edu.cscc.framework.ApplicationController;
 import edu.cscc.framework.MVCContext;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 import java.util.Properties;
 
 public class OrdersController extends ApplicationController {
@@ -23,12 +22,51 @@ public class OrdersController extends ApplicationController {
         super(context);
     }
 
-    public void index() {
-        render(new OrdersIndex(context));
+    public void index() throws SQLException {
+            Properties properties = new Properties();
+            buildProperties(properties);
+            DataSource dataSource = getDataSource(properties);
+            Connection connection = null;
+            try {
+                connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select ol.order_id, e.first_name, e.last_name, c.smart_id, c.first_name, c.last_name, orders.store_number, r.name\n" +
+                    "from lackluster_video.order_line_items ol\n" +
+                    "inner join lackluster_video.orders orders on ol.order_id = orders.id\n" +
+                    "inner join lackluster_video.customers c on orders.customer_id = c.id\n" +
+                    "inner join lackluster_video.employees e on orders.employee_id = e.id\n" +
+                    "inner JOIN lackluster_video.rentals r on ol.rental_id = r.id");
+                render(new OrdersIndex(context, resultSet));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        }
+
+    private static void buildProperties(Properties properties) {
+        properties.setProperty(MYSQL_DB_URL, "jdbc:mysql://localhost:3306/lackluster_video");
+        properties.setProperty(MYSQL_DB_USERNAME, "root");
+        properties.setProperty(MYSQL_DB_PASSWORD, "password");
     }
 
     public void delete() {
-        render(new OrdersIndex(context));
+        Properties properties = new Properties();
+        buildProperties(properties);
+        DataSource dataSource = getDataSource(properties);
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select ol.order_id, e.first_name, e.last_name, c.smart_id, c.first_name, c.last_name, orders.store_number, r.name\n" +
+                    "from lackluster_video.order_line_items ol\n" +
+                    "inner join lackluster_video.orders orders on ol.order_id = orders.id\n" +
+                    "inner join lackluster_video.customers c on orders.customer_id = c.id\n" +
+                    "inner join lackluster_video.employees e on orders.employee_id = e.id\n" +
+                    "inner JOIN lackluster_video.rentals r on ol.rental_id = r.id");
+            render(new OrdersIndex(context, resultSet));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void create() {
@@ -36,10 +74,6 @@ public class OrdersController extends ApplicationController {
     }
 
     public void complete() {
-        render(new OrdersIndex(context));
-    }
-
-    public void save() {
         // Todo save order to database
         Properties properties = new Properties();
         properties.setProperty(MYSQL_DB_URL, "jdbc:mysql://localhost:3306/lackluster_video");
@@ -47,20 +81,43 @@ public class OrdersController extends ApplicationController {
         properties.setProperty(MYSQL_DB_PASSWORD, "password");
         currentEmployeeId = context.getRequest().getParams().get("employeeId").toString();
         currentCustomerId = context.getRequest().getParams().get("customerid").toString();
+        List<String> rentalIds = (List <String>) context.getRequest().getParams().get("rentalIds");
         DataSource dataSource = getDataSource(properties);
         String insert = "Insert into orders (employee_id, customer_id, store_number) values (" + currentEmployeeId + ", " + currentCustomerId + ", (SELECT active_store_number FROM employees WHERE id = " + currentEmployeeId + "))";
 
         try {
             Connection connection = dataSource.getConnection();
-            PreparedStatement updateOrders = connection.prepareStatement(insert);
-            updateOrders.execute();
+            connection.setAutoCommit(false);
+            PreparedStatement updateOrders = connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            updateOrders.executeUpdate();
+            ResultSet keySet = updateOrders.getGeneratedKeys();
+            keySet.next();
+            for (String rentalId : rentalIds) {
+                String insert2 = "Insert into order_line_items (order_id, rental_id) values (" + keySet.getInt(1) + ", " + rentalId + ")";
+                PreparedStatement updateLineItems = connection.prepareStatement(insert2);
+                updateLineItems.executeUpdate();
+            }
+            connection.commit();
             connection.close();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        render(new OrdersCreate(context));
+        try {
+            Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select ol.order_id, e.first_name, e.last_name, c.smart_id, c.first_name, c.last_name, orders.store_number, r.name\n" +
+                    "from lackluster_video.order_line_items ol\n" +
+                    "inner join lackluster_video.orders orders on ol.order_id = orders.id\n" +
+                    "inner join lackluster_video.customers c on orders.customer_id = c.id\n" +
+                    "inner join lackluster_video.employees e on orders.employee_id = e.id\n" +
+                    "inner JOIN lackluster_video.rentals r on ol.rental_id = r.id");
+            render(new OrdersIndex(context, resultSet));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
+
 
     private static DataSource getDataSource(Properties properties) {
         MysqlDataSource dataSource = new MysqlDataSource();
--- src/edu/cscc/OrdersCreate.java
+++ src/edu/cscc/OrdersCreate.java
@@ -3,10 +3,7 @@ package edu.cscc;
 import edu.cscc.framework.ApplicationView;
 import edu.cscc.framework.MVCContext;
 
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class OrdersCreate extends ApplicationView {
 
@@ -31,33 +28,38 @@ public class OrdersCreate extends ApplicationView {
         System.out.println("Customer id: ");
         String customerid = scanner.nextLine();
         params.put("customerid", customerid);
-        System.out.println("1. Add a rental");
-        System.out.println("2. Complete order");
-        System.out.println("3. Manage orders");
-        System.out.println("Choice:");
-
-        try {
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    scanner.nextLine();
-                    System.out.println("Rental id: ");
-                    String rentalId = scanner.nextLine();
-                    params.put("rentalId", rentalId);
+        int choice = 0;
+        List<String> rentalIds = new ArrayList<>();
+        do {
 
 
-                    route("Orders", "save", params);
-                    break;
-                case 2:
-                    route("Orders", "complete");
-                    break;
-                case 3:
-                    route("Orders", "index");
-                    break;
+            System.out.println("1. Add a rental");
+            System.out.println("2. Complete order");
+            System.out.println("3. Manage orders");
+            System.out.println("Choice:");
+
+            try {
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        scanner.nextLine();
+                        System.out.println("Rental id: ");
+                        String rentalId = scanner.nextLine();
+                        rentalIds.add(rentalId);
+
+                        break;
+                    case 2:
+                        params.put("rentalIds", rentalIds);
+                        route("Orders", "complete", params);
+                        break;
+                    case 3:
+                        route("Orders", "index");
+                        break;
+                }
+            } catch (InputMismatchException ex) {
+                route("Orders", "index");
+
             }
-        } catch (InputMismatchException ex) {
-            route("Orders", "index");
-
-        }
+        } while (choice == 1);
     }
 }
